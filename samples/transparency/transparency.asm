a92d7c4649730d60eb842d4878854aee4d62cf31
JF Dusariez <jean-francois.dusariez@wikipower.be>
2020-02-20 18:51:38 +0100

    fix transparency

--- samples/transparency/transparency.asm
+++ samples/transparency/transparency.asm
@@ -33,29 +33,36 @@ BACKGROUND_MAP_ADDR   = $1000
 .segment "RODATA"
 
 spriteTiles:
-    .incbin "resource/main-sprite.pic"
+    ;.incbin "resource/main-sprite.pic"
+    .incbin "resource/black.pic"
 
 spritePal:
-    .incbin "resource/main-sprite.clr"
+    ;.incbin "resource/main-sprite.clr"
+    .incbin "resource/black.clr"
 
 shadowSpriteTiles:
-    .incbin "resource/main-shadow-sprite.pic"
+    ;.incbin "resource/main-shadow-sprite.pic"
     ;.incbin "resource/main-shadow-sprite-inverted.pic"
+    .incbin "resource/black-shadow.pic"
 
 shadowSpritePal:
-    .incbin "resource/main-shadow-sprite.clr"
+    ;.incbin "resource/main-shadow-sprite.clr"
     ;.incbin "resource/main-shadow-sprite-inverted.clr"
+    .incbin "resource/black-shadow.clr"
 
 backgroundMap:
-    .incbin "resource/background.map"
+    ;.incbin "resource/background.map"
+    .incbin "resource/background-checker.map"
 
 backgroundPal:
-    .incbin "resource/background.clr"
+    ;.incbin "resource/background.clr"
+    .incbin "resource/background-checker.clr"
 
 .segment "BANK1"
 
 backgroundTiles:
-    .incbin "resource/background.pic"
+    ;.incbin "resource/background.pic"
+    .incbin "resource/background-checker.pic"
 
 .segment "BSS"
 
@@ -90,31 +97,36 @@ oamData:
 
     ;*** Setup sprites ***
 
-    VRAMLoad spriteTiles, SPRITE_TILE_ADDR, $0100
-    CGRAMLoad spritePal, $80, $10
+    VRAMLoad spriteTiles, SPRITE_TILE_ADDR, $0120
+    CGRAMLoad spritePal, $80, $20
 
-    VRAMLoad shadowSpriteTiles, SHADOW_TILE_ADDR, $0100
+    VRAMLoad shadowSpriteTiles, SHADOW_TILE_ADDR, $00c0
     CGRAMLoad shadowSpritePal, $c0, $20
 
     lda #$01         ; setBGMODE(0, 0, 1);
     sta $2105
 
-    lda #$01         ; main screen definition (OAM+BG1)
+    lda #$11         ; main screen definition (OAM)
     sta $212c
 
-    lda #$10         ; sub screen definition
+    lda #$01         ; sub screen definition (BG1)
     sta $212d
 
-    lda #%00000010
+    lda #%00000010   ; enable add/sub from subscreen
     sta $2130
 
-    lda #%01000001   ; 0 -> Sub / 1 -> Add
-                     ; 0 -> 1/2 Disable / 1 -> 1/2 Enable
-
-                     ; Currently I have a better effect with 1/2 enabled
+    lda #%01110001   ; 0 -> Sub / 1 -> Add
+                     ; 0 -> 1/2 Disable / *1* -> 1/2 Enable
+                     ; *back* / *oam* / bg4 / bg3 / bg2 / *bg1*
+                     ; ps : I enable bg1 because of halving so subscreen keep
+                     ; the right color. BG1 need a #0 color set to pure black
+                     ; use --color-zero 00000000 with superfamiconv
     sta $2131
 
     lda #$00
+    sta $2132
+
+    lda #$e0         ; Set black for fixed color
     sta $2132
 
     jsr spriteInit
@@ -177,61 +189,60 @@ spriteDataClear:
     ;*** Start main Sprite ***
     ;*************************
 
-/*
-    lda #$64
+    lda #$60
     sta oamData+32                     ; H pos of the first sprite
-    lda #$64
+    lda #$60
     sta oamData+33                  ; V pos of the first sprite
     lda #$00
     sta oamData+34
     lda #%00110000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+35
 
-    lda #$64
+    lda #$68
     sta oamData+36                   ; H pos of the first sprite
-    lda #$6c
+    lda #$60
     sta oamData+37                  ; V pos of the first sprite
-    lda #$02
+    lda #$01
     sta oamData+38
     lda #%00110000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+39
 
-    lda #$64
+    lda #$60
     sta oamData+40                   ; H pos of the first sprite
-    lda #$74
+    lda #$68
     sta oamData+41                   ; V pos of the first sprite
-    lda #$04
+    lda #$02
     sta oamData+42
     lda #%00110000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+43
 
-    lda #$64
+    lda #$68
     sta oamData+44                   ; H pos of the first sprite
-    lda #$7c
+    lda #$68
     sta oamData+45                   ; V pos of the first sprite
-    lda #$06
+    lda #$03
     sta oamData+46
     lda #%00110000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+47
 
-    lda #$6c
+    lda #$60
     sta oamData+16                     ; H pos of the first sprite
-    lda #$64
+    lda #$70
     sta oamData+17                   ; V pos of the first sprite
-    lda #$01
+    lda #$04
     sta oamData+18
     lda #%00110000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+19
 
-    lda #$6c
+    lda #$68
     sta oamData+20                   ; H pos of the first sprite
-    lda #$6c
+    lda #$70
     sta oamData+21                   ; V pos of the first sprite
-    lda #$03
+    lda #$05
     sta oamData+22
     lda #%00110000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+23
-
+/*
     lda #$6c
     sta oamData+24                   ; H pos of the first sprite
     lda #$74
@@ -249,13 +260,13 @@ spriteDataClear:
     sta oamData+30
     lda #%00110000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+31
-
+*/
     lda #%00000000
     sta oamData + $200
 
     lda #%00000000
     sta oamData + $201
-*/
+
     ;***********************
     ;*** End main Sprite ***
     ;***********************
@@ -264,56 +275,56 @@ spriteDataClear:
     ;*** Start shadow Sprite ***
     ;***************************
 
-    lda #$64
+    lda #$5b
     sta oamData                     ; H pos of the first sprite
-    lda #$64
+    lda #$65
     sta oamData+1                   ; V pos of the first sprite
     lda #$10
     sta oamData+2
     lda #%00111000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+3
 
-    lda #$6c
+    lda #$63
     sta oamData+4                   ; H pos of the first sprite
-    lda #$64
+    lda #$65
     sta oamData+5                   ; V pos of the first sprite
     lda #$11
     sta oamData+6
     lda #%00111000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+7
 
-    lda #$64
+    lda #$6b
     sta oamData+8                   ; H pos of the first sprite
-    lda #$6c
+    lda #$65
     sta oamData+9                   ; V pos of the first sprite
     lda #$12
     sta oamData+10
     lda #%00111000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+11
 
-    lda #$6c
+    lda #$5b
     sta oamData+12                   ; H pos of the first sprite
-    lda #$6c
+    lda #$6d
     sta oamData+13                   ; V pos of the first sprite
     lda #$13
     sta oamData+14
     lda #%00111000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+15
 
-    lda #$64
+    lda #$63
     sta oamData+48                     ; H pos of the first sprite
-    lda #$74
+    lda #$6d
     sta oamData+49                   ; V pos of the first sprite
-    lda #$10
+    lda #$14
     sta oamData+50
     lda #%00111000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+51
 
-    lda #$6c
+    lda #$6b
     sta oamData+52                   ; H pos of the first sprite
-    lda #$74
+    lda #$6d
     sta oamData+53                   ; V pos of the first sprite
-    lda #$11
+    lda #$15
     sta oamData+54
     lda #%00111000                  ; no flip full priority palette 0 (8 global palette)
     sta oamData+55
